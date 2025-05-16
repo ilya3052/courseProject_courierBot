b9bb85417b3939138b663ae47e9e274e439bed29
ilya3052 <ipopadin5@gmail.com>
2025-05-16 23:51:26 +0300

    ref: подключение роутеров перенесено внутрь функции

--- bot.py
+++ bot.py
@@ -2,12 +2,11 @@ import logging
 from handlers import profile_router, reg_router, deliveries_router
 from bot_instance import bot, dp
 
-dp.include_router(reg_router)
-dp.include_router(profile_router)
-dp.include_router(deliveries_router)
-
-
 async def setup_bot():
+    dp.include_router(reg_router)
+    dp.include_router(profile_router)
+    dp.include_router(deliveries_router)
+
     logging.info("Бот запущен")
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
