4b44df6ad967d5ec7fc897d2bde9f968db965dd7
ilya3052 <ipopadin5@gmail.com>
2025-05-16 23:51:56 +0300

    add: подключение переменных окружения через load_dotenv

--- bot_instance.py
+++ bot_instance.py
@@ -2,7 +2,9 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from dotenv import load_dotenv
 
+load_dotenv()
 
 bot = Bot(
     token=os.getenv("BOT_TOKEN"),
