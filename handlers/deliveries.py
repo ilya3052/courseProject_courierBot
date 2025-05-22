72f32cab225d153490e8a16fe6dd5d4a23bf4fee
ilya3052 <ipopadin5@gmail.com>
2025-05-22 22:37:42 +0300

    add: функция для обработки уведомлений от канала low_rating ref: замена format на плейсхолдеры ref: замена запросов на вызов хранимых функций

--- handlers/deliveries.py
+++ handlers/deliveries.py
@@ -5,6 +5,7 @@ from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import CallbackQuery, Message
+from icecream import ic
 from psycopg import sql
 from psycopg.errors import LockNotAvailable
 
@@ -27,20 +28,37 @@ async def get_notify(conn, pid, channel, payload):
     await send_notify(order_id)
 
 
+async def low_rating(conn, pid, channel, payload):
+    print(f"[{channel}] => {payload}")
+    connect: ps.connect = Database.get_connection()
+    try:
+        with connect.cursor() as cur:
+            user = cur.execute("""SELECT u.user_tgchat_id 
+            FROM users u 
+            JOIN courier c ON c.user_id = u.user_id 
+            WHERE c.courier_id = %s""",
+                               (payload,)
+                               ).fetchone()[0]
+        await bot.send_message(user,
+                               "Ваш рейтинг опустился ниже допустимого значения, возможность принимать заказы временно заблокирована. Обратитесь к администратору!")
+    except ps.Error as p:
+        logging.exception(f"Произошла ошибка при выполнении запроса: {p}")
+
+
 async def send_notify(order_id: int):
     connect: ps.connect = Database.get_connection()
 
     free_couriers = await get_free_couriers()
 
     if not free_couriers:
-        await Database.notify_channel("order_not_accept", f"action: order_not_accept; order_id: {order_id}")
+        await Database.notify_channel("order_status", f"action: order_not_accept; order_id: {order_id}")
         return
 
     get_product_info = (sql.SQL(
         """SELECT COUNT(*), o.order_address 
 FROM \"order\" o 
     JOIN added a on o.order_id = a.order_id 
-    WHERE o.order_id = {}
+    WHERE o.order_id = %s
 GROUP BY o.order_address;"""
     ))
 
@@ -48,7 +66,7 @@ GROUP BY o.order_address;"""
 FROM product p 
 	JOIN added a on a.product_article = p.product_article 
 	JOIN "order" o ON o.order_id = a.order_id 
-WHERE o.order_id = {}
+WHERE o.order_id = %s
 GROUP BY p.product_name, p.product_price;
         """))
 
@@ -56,13 +74,13 @@ GROUP BY p.product_name, p.product_price;
 FROM product p 
     JOIN added a ON a.product_article = p.product_article 
     JOIN \"order\" o ON o.order_id = a.order_id 
-WHERE o.order_id = {};"""))
+WHERE o.order_id = %s;"""))
 
     with connect.cursor() as cur:
         try:
-            product_info = cur.execute(get_product_info.format(order_id)).fetchone()
-            product_total_price = cur.execute(get_product_total_price.format(order_id)).fetchone()[0]
-            products_list = cur.execute(get_product_list.format(order_id)).fetchall()
+            product_info = cur.execute(get_product_info, (order_id,)).fetchone()
+            product_total_price = cur.execute(get_product_total_price, (order_id,)).fetchone()[0]
+            products_list = cur.execute(get_product_list, (order_id,)).fetchall()
         except ps.Error as p:
             logging.exception(f"Произошла ошибка при выполнении запроса: {p}")
 
@@ -80,38 +98,21 @@ WHERE o.order_id = {};"""))
                                reply_markup=get_order_notify_kb(order_id))
 
 
-# ОФОРМИТЬ КАК ТРАНЗАКЦИЮ ВНУТРИ ПОСТГРЕСА
 @router.callback_query(F.data.startswith("action_accept"), IsRegistered())
-async def order_accept_handler(callback: CallbackQuery, state: FSMContext):
+async def order_accept_handler(callback: CallbackQuery):
     connect: ps.connect = Database.get_connection()
 
     order_id = callback.data.split(":")[1]
 
-    get_courier_id = (sql.SQL(
-        "SELECT c.courier_id FROM courier c JOIN users u ON c.user_id = u.user_id WHERE u.user_tgchat_id = {} AND u.user_role = 'courier'"
-    ))
     try:
         with connect.cursor() as cur:
-            courier_id = cur.execute(get_courier_id.format(callback.message.chat.id)).fetchone()[0]
-    except ps.Error as p:
-        logging.exception(f"Произошла ошибка при выполнении запроса: {p}")
-
-    try:
-        with connect.cursor() as cur:
-            order_status = cur.execute(
-                "SELECT order_status FROM \"order\" WHERE order_id = {} FOR UPDATE NOWAIT;".format(
-                    order_id)).fetchone()[0]
-            if order_status is None or order_status != 0:
-                await callback.answer("Невозможно принять этот заказ!")
-                connect.rollback()
-                return
-            cur.execute("INSERT INTO delivery (courier_id, order_id) VALUES ({}, {});".format(courier_id, order_id))
-            cur.execute(
-                "UPDATE courier SET courier_is_busy_with_order = true WHERE courier_id = {};".format(courier_id))
-            cur.execute("UPDATE \"order\" SET order_status = 1 WHERE order_id = {};".format(order_id))
-            connect.commit()
-            await Database.notify_channel("order_status", f'action: order_accept; order_id: {order_id}')
+            status = cur.execute("SELECT accept_order(%s, %s)", (callback.message.chat.id, order_id,)).fetchone()[0]
+            ic(status)
+            if status == 1:
+                raise LockNotAvailable()
             await callback.answer()
+            await Database.notify_channel("order_status", f'action: order_accept; order_id: {order_id}')
+            connect.commit()
     except LockNotAvailable:
         connect.rollback()
         await callback.answer("Заказ уже принят другим курьером!")
