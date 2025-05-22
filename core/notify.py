ae5df1e2e69b2aec04a1f6b14f2775dcbaf00a69
ilya3052 <ipopadin5@gmail.com>
2025-05-22 22:36:17 +0300

    add: прослушка канала для уведомлений о низком рейтинге

--- core/notify.py
+++ core/notify.py
@@ -1,6 +1,7 @@
+from handlers.deliveries import get_notify, low_rating
 from .database import Database
-from handlers.deliveries import get_notify
 
 
 async def setup_notifications():
     await Database.listen_channel("create_order", get_notify)
+    await Database.listen_channel("low_rating", low_rating)
